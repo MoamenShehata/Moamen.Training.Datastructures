c6c1be30aa80079140a9726a9383e0902b9c6a32
Moamen Shehata <engmoamen00@gmail.com>
2022-07-03 15:46:59 +0200

    Added LinkedList class | Implemented AddLast

---
+++ Moamen.Training.DataStructures.LinkedList/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moamen.Training.DataStructures.LinkedList
+{
+    public class LinkedList<T>
+    {
+        public LinkedListNode<T> Head { get; set; }
+        public LinkedListNode<T> Tail { get; set; }
+        public int Count { get; private set; }
+
+        public void AddLast(LinkedListNode<T> node)
+        {
+            if (Count == 0)
+            {
+                Head = Tail = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                Tail = node;
+            }
+            
+            Count++;
+        }
+    }
+}
