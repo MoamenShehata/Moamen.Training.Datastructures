9ee8353133a593295015794b01057bbeef8492f7
Moamen Shehata <engmoamen00@gmail.com>
2022-07-04 10:26:43 +0200

    Added Some Tests

---
+++ Moamen.Training.DataStructures.Tests/LinkedListTests.cs
@@ -0,0 +1,44 @@
+namespace Moamen.Training.DataStructures.Tests
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void AddLast()
+        {
+            var ll = new LinkedList.LinkedList<int>();
+
+            ll.AddLast(1);
+
+            Assert.Equal(ll.Head.Value, 1);
+
+            Assert.NotEqual(ll.Tail,null);
+        }
+
+        [Fact]
+        public void AddFirst()
+        {
+            var ll = new LinkedList.LinkedList<int>();
+
+            ll.AddFirst(2);
+
+            Assert.Equal(ll.Head.Value, 2);
+
+            Assert.Equal(ll.Tail.Value,2);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var ll = new LinkedList.LinkedList<int>();
+
+            ll.AddFirst(1);
+            ll.AddLast(2);
+
+            Assert.Equal(ll.Head.Value, 1);
+
+            Assert.Equal(ll.Tail.Value,2);
+        }
+
+
+    }
+}
