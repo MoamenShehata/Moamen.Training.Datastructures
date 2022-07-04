ab6d4c89a6081fbc68a23b3507db98ac3bcfc644
Moamen Shehata <engmoamen00@gmail.com>
2022-07-04 12:35:31 +0200

    Added AStack: Stack using array

---
+++ Moamen.Training.DataStructures.Stack/AStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moamen.Training.DataStructures.Stack
+{
+    public class AStack<T> : IEnumerable<T>
+    {
+        private T[] stack;
+        private int count = 0;
+
+        public AStack(int capacity = 10)
+        {
+            stack = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            if (count == stack.Length)
+            {
+                var newArr = new T[stack.Length * 2];
+                stack.CopyTo(newArr, 0);
+                stack = newArr;
+            }
+            stack[count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (count == 0)
+                throw new Exception("The stack is empty");
+            return stack[--count];
+        }
+
+        public T Peek()
+        {
+            if (count == 0)
+                throw new Exception("The stack is empty");
+            return stack[count - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = count - 1; i >= 0; i--)
+            {
+                yield return stack[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
--- Moamen.Training.DataStructures.Stack/Program.cs
+++ Moamen.Training.DataStructures.Stack/Program.cs
@@ -2,16 +2,38 @@
 using Moamen.Training.DataStructures.Stack;
 
 Console.WriteLine("Hello, World!");
-var st = new LStack<int>();
+//var st = new LStack<int>();
+//st.Push(5);
+//st.Push(4);
+//st.Push(3);
+//st.Push(2);
+//st.Push(1);
+
+//foreach (var item in st)
+//{
+//    Console.WriteLine(item);
+//}
+//Console.WriteLine(st.Peek());
+
+var st = new AStack<int>(6);
+st.Push(6);
 st.Push(5);
 st.Push(4);
 st.Push(3);
 st.Push(2);
 st.Push(1);
+st.Push(0);
+st.Push(-1);
+
+Console.WriteLine(st.Count());
+Console.WriteLine("////////////////////// Popping //////////////////////");
+Console.WriteLine(st.Pop());
+Console.WriteLine("////////////////////// Popping //////////////////////");
+Console.WriteLine(st.Count());
 
 foreach (var item in st)
 {
     Console.WriteLine(item);
 }
-Console.WriteLine(st.Peek());
+
 Console.Read();
