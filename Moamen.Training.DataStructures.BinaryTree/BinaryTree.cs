b5e896b98b0fe0759e777a62440dee6326464d5b
Moamen Shehata <engmoamen00@gmail.com>
2022-07-05 10:59:59 +0200

    Implemented BinaryTree.Add()

---
+++ Moamen.Training.DataStructures.BinaryTree/BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moamen.Training.DataStructures.BinaryTree
+{
+    public class BinaryTree<T>
+        where T : IComparable<T>
+    {
+        private BinaryTreeNode<T> root;
+        private int count;
+
+        public void Add(T value)
+        {
+            if (count == 0)
+            {
+                root = new BinaryTreeNode<T>(value);
+            }
+            else
+            {
+                AddTo(root, value);
+            }
+
+            count++;
+        }
+
+        private void AddTo(BinaryTreeNode<T> node, T value)
+        {
+            if (value.CompareTo(node.Value) < 0)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddTo(node.Left, value);
+                }
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddTo(node.Right, value);
+                }
+            }
+        }
+    }
+}
---
+++ Moamen.Training.DataStructures.BinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moamen.Training.DataStructures.BinaryTree
+{
+    public class BinaryTreeNode<T> : IComparable<T>
+        where T : IComparable<T>
+    {
+        public T Value { get; private set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+}
